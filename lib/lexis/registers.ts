f03e5c92d7400838eb1bdb0dbf8902e743e59888
AJ Whatling <riojack@users.noreply.github.com>
2019-02-02 18:51:23 -0600

    Add lexis

---
+++ lib/lexis/registers.ts
@@ -0,0 +1,13 @@
+class Registers {
+  // General purpose
+  public static readonly RA: string = "RA";
+  public static readonly RB: string = "RB";
+  public static readonly RC: string = "RC";
+  public static readonly RD: string = "RD";
+  public static readonly RE: string = "RE";
+  public static readonly RF: string = "RF";
+  public static readonly RG: string = "RG";
+  public static readonly RH: string = "RH";
+  // Arithmetic
+  public static readonly AR: string = "AR";
+}
