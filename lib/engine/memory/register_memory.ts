2cf7e74955308dd084cca9894fb22506b4b32aab
AJ Whatling <riojack@users.noreply.github.com>
2019-02-06 19:19:02 -0600

    Stub out register memory.

---
+++ lib/engine/memory/register_memory.ts
@@ -0,0 +1,10 @@
+class RegisterMemory {
+  write(address: number, bytes: number): void {
+    throw new Error("Method not implemented.");
+  }
+  read(address: number): number {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export default RegisterMemory;
