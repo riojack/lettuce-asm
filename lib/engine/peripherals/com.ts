79c0edc42508d1db49fbe05229b15e9464003875
riojack <ayjay122@gmail.com>
2020-01-21 14:20:19 -0600

    misc: add Com class with peripherals

---
+++ lib/engine/peripherals/com.ts
@@ -0,0 +1,7 @@
+class Com {
+  public static readonly COM_1_BLOCK: number = 0x0300;
+  public static readonly COM_2_BLOCK: number = 0x0320;
+  public static readonly COM_3_BLOCK: number = 0x0340;
+}
+
+export default Com;
