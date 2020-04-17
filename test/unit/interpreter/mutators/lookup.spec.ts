81ab3d3311738d705e2cedf0afeb687d6ba067c0
AJ Whatling <riojack@users.noreply.github.com>
2020-04-16 21:05:29 -0500

    test: MutatorLookup contains look-up for increment mutator

--- test/unit/interpreter/mutators/lookup.spec.ts
+++ test/unit/interpreter/mutators/lookup.spec.ts
@@ -1,11 +1,17 @@
 import * as chai from 'chai';
-import { MutatorLookup } from '../../../../lib/interpreter/mutators/lookup';
-import { addMutator } from '../../../../lib/interpreter/mutators/functions/addMutator';
+import {MutatorLookup} from '../../../../lib/interpreter/mutators/lookup';
+import {addMutator} from '../../../../lib/interpreter/mutators/functions/addMutator';
+import {incrementMutator} from '../../../../lib/interpreter/mutators/functions/incrementMutator';
 import Opcodes from '../../../../lib/lexis/opcodes';
 
 describe('interpreter mutators', (): void => {
   it('should support the ADD opcode', (): void => {
     chai.expect(MutatorLookup).to.include.keys(Opcodes.ADD);
     chai.expect(MutatorLookup[Opcodes.ADD]).to.equal(addMutator);
+  });
+
+  it('should support the INC opcode', (): void => {
+    chai.expect(MutatorLookup).to.include.keys(Opcodes.INC);
+    chai.expect(MutatorLookup[Opcodes.INC]).to.equal(incrementMutator);
   });
 });
