e2cc5ddfc89bbce00e71ca34401a506f6657fa08
AJ Whatling <riojack@users.noreply.github.com>
2020-04-16 21:12:29 -0500

    test: acceptance for INC instruction

--- test/acceptance/arithmetic.spec.ts
+++ test/acceptance/arithmetic.spec.ts
@@ -18,4 +18,18 @@ describe('Arithmetic', (): void => {
         .to.eql(0x3);
     });
   });
+
+  describe('given the program ADD RA, 0x13 followed by INC RA, followed by INC RA, when executed...', () => {
+    it('then register RA will hold a value equal to Ox15', (): void => {
+      const program = 'ADD RA, 0x13\nINC RA\nINC RA';
+
+      const interpreter: Interpreter = new Interpreter();
+      const terminalState: TerminalState = interpreter.execute(program);
+
+      const registerRaValue: number = terminalState.readRegister(Registers.RA);
+
+      chai.expect(registerRaValue)
+        .to.eql(0x15);
+    });
+  });
 });
