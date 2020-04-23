13a9029fc1a7b31eb71d35adc7a738f63f20e39b
AJ Whatling <riojack@users.noreply.github.com>
2020-04-22 21:20:18 -0500

    test: DIV stores quotient in first register

--- lib/interpreter/mutators/functions/divMutator.ts
+++ lib/interpreter/mutators/functions/divMutator.ts
@@ -1,7 +1,16 @@
 import {MutatorFunc} from '../lookup';
 import IParseNode from '../../../parser/nodes/parse_node';
 import TerminalState from '../../terminal_state';
+import RegisterMemoryFacade from '../../../engine/memory_facades/register_memory_facade';
 
 export const divMutator: MutatorFunc = (node: IParseNode, previousState: TerminalState): TerminalState => {
-  return null;
+  const memory: RegisterMemoryFacade = previousState.registerMemory;
+  const [register, bytes]: [string, number] = node.getOperands() as [string, number];
+  const currentValueInRegister: number = memory.read(register);
+
+  const nextValueForRegister = ~~(currentValueInRegister / bytes);
+
+  memory.write(register, nextValueForRegister);
+
+  return new TerminalState(memory);
 };
---
+++ test/unit/interpreter/mutators/functions/divMutator.spec.ts
@@ -0,0 +1,36 @@
+import * as chai from 'chai';
+import RegisterMemory from '../../../../../lib/engine/memory/register_memory';
+import RegisterMemoryFacade from '../../../../../lib/engine/memory_facades/register_memory_facade';
+import TerminalState from '../../../../../lib/interpreter/terminal_state';
+import IParseNode from '../../../../../lib/parser/nodes/parse_node';
+import InstructionNode from '../../../../../lib/parser/nodes/instruction_node';
+import Opcodes from '../../../../../lib/lexis/opcodes';
+import Registers from '../../../../../lib/lexis/registers';
+import {MutatorFunc, MutatorLookup} from '../../../../../lib/interpreter/mutators/lookup';
+
+describe('DIV mutator', (): void => {
+  let memory: RegisterMemory;
+  let memoryFacade: RegisterMemoryFacade;
+  let previousState: TerminalState;
+
+  beforeEach((): void => {
+    memory = new RegisterMemory();
+    memoryFacade = new RegisterMemoryFacade(memory);
+    previousState = new TerminalState(memoryFacade);
+  });
+
+  context('for program "ADD RA, 0x6" followed by "DIV RA, 0x2"', (): void => {
+    it('should return a terminal state with register RA set to three', (): void => {
+      const addNode: IParseNode = new InstructionNode(Opcodes.ADD, '', [Registers.RA, 0x6]);
+      const instructionNode: IParseNode = new InstructionNode(Opcodes.DIV, '', [Registers.RA, 0x2]);
+      const addMutatorFunc: MutatorFunc = MutatorLookup[Opcodes.ADD];
+      const divMutatorFunc: MutatorFunc = MutatorLookup[Opcodes.DIV];
+
+      const firstState: TerminalState = addMutatorFunc(addNode, previousState);
+      const secondState: TerminalState = divMutatorFunc(instructionNode, firstState);
+
+      chai.expect(secondState.readRegister(Registers.RA))
+        .to.equal(0x3);
+    });
+  });
+});
