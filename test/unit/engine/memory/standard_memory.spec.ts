4fb616d351fe082842188cc2a346e15fc394256d
AJ Whatling <riojack@users.noreply.github.com>
2020-05-10 17:43:37 -0500

    refactor: simplify standard memory tests

--- test/unit/engine/memory/standard_memory.spec.ts
+++ test/unit/engine/memory/standard_memory.spec.ts
@@ -3,16 +3,20 @@ import StandardMemory from '../../../../lib/engine/memory/standard_memory';
 import * as chai from 'chai';
 
 describe('standard memory', (): void => {
-  it('should fail to write beyond 0x3FFFFF', () => {
-    const mem: StandardMemory = new StandardMemory();
+  let mem: StandardMemory;
+
+  beforeEach((): void => {
+    mem = new StandardMemory();
+  });
+
+  it('should fail to write beyond 0x3FFFFF', (): void => {
     const address = 0x1000000B;
 
     chai.expect((): void => mem.write(address, 0x1))
       .to.throw('Cannot write to memory beyond 0x3FFFFF.');
   });
 
-  it('should fail to write to locations less than 0x0', () => {
-    const mem: StandardMemory = new StandardMemory();
+  it('should fail to write to locations less than 0x0', (): void => {
     const address = -0x4DA4404;
 
     chai.expect((): void => mem.write(address, 0x1))
@@ -20,7 +24,6 @@ describe('standard memory', (): void => {
   });
 
   it('should read back what was written to standard memory', (): void => {
-    const mem: StandardMemory = new StandardMemory();
     const address = 0x000C0000;
     const bytes = 0x87003200;
 
@@ -31,22 +34,18 @@ describe('standard memory', (): void => {
   });
 
   it('should fail to read from negative locations in standard memory', () => {
-    const mem: StandardMemory = new StandardMemory();
     const address = -0x000C0000;
 
     chai.expect((): void => {
       mem.read(address);
-    })
-      .to.throw('Cannot read from a negative memory location.  Hah!');
+    }).to.throw('Cannot read from a negative memory location.  Hah!');
   });
 
   it('should fail to read from locations beyond 0x3FFFFF in standard memory', () => {
-    const mem: StandardMemory = new StandardMemory();
     const address = 0x412345;
 
     chai.expect((): void => {
       mem.read(address);
-    })
-      .to.throw('Cannot read beyond 0x3FFFFF.  Really, there\'s nothing there.');
+    }).to.throw('Cannot read beyond 0x3FFFFF.  Really, there\'s nothing there.');
   });
 });
