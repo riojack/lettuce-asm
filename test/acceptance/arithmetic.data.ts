20549c663e55ee2b8cf663f64c1464dd0f6d019c
AJ Whatling <riojack@users.noreply.github.com>
2020-05-10 21:23:36 -0500

    refactor: pull out acceptance test Lettuce programs

---
+++ test/acceptance/arithmetic.data.ts
@@ -0,0 +1,11 @@
+export const TRIPLE_ADD_PROGRAM = [
+  'ADD RA, 0x1',
+  'ADD RA, 0x1',
+  'ADD RA, 0x1'
+].join('\n');
+
+export const INCREMENT_PROGRAM = [
+  'ADD RA, 0x13',
+  'INC RA',
+  'INC RA'
+].join('\n');
--- test/acceptance/arithmetic.spec.ts
+++ test/acceptance/arithmetic.spec.ts
@@ -1,34 +1,32 @@
 import Interpreter from '../../lib/interpreter/interpreter';
 import TerminalState from '../../lib/interpreter/terminal_state';
 
-import * as chai from 'chai';
+import { expect } from 'chai';
 import Registers from '../../lib/lexis/registers';
 
-describe('Arithmetic', (): void => {
-  describe('given the program ADD RA, 0x1 repeated 3 times, when executed...', (): void => {
-    it('then register RA will hold a value equal to Ox3', (): void => {
-      const program = 'ADD RA, 0x1\nADD RA, 0x1\nADD RA, 0x1';
+import { TRIPLE_ADD_PROGRAM, INCREMENT_PROGRAM } from './arithmetic.data';
 
+describe('Arithmetic', (): void => {
+  describe(`given the program\n${TRIPLE_ADD_PROGRAM}\nwhen executed...`, (): void => {
+    it('then register RA will hold a value equal to Ox3', (): void => {
       const interpreter: Interpreter = new Interpreter();
-      const terminalState: TerminalState = interpreter.execute(program);
+      const terminalState: TerminalState = interpreter.execute(TRIPLE_ADD_PROGRAM);
 
       const registerRaValue: number = terminalState.readRegister(Registers.RA);
 
-      chai.expect(registerRaValue)
+      expect(registerRaValue)
         .to.eql(0x3);
     });
   });
 
-  describe('given the program ADD RA, 0x13 followed by INC RA, followed by INC RA, when executed...', () => {
+  describe(`given the program\n${INCREMENT_PROGRAM}\nwhen executed...`, () => {
     it('then register RA will hold a value equal to Ox15', (): void => {
-      const program = 'ADD RA, 0x13\nINC RA\nINC RA';
-
       const interpreter: Interpreter = new Interpreter();
-      const terminalState: TerminalState = interpreter.execute(program);
+      const terminalState: TerminalState = interpreter.execute(INCREMENT_PROGRAM);
 
       const registerRaValue: number = terminalState.readRegister(Registers.RA);
 
-      chai.expect(registerRaValue)
+      expect(registerRaValue)
         .to.eql(0x15);
     });
   });
